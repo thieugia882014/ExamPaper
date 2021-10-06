620118c593acd929cb08ab2d6cff41a1cf7931f5
Tuấn Diệp <minhtuanmk1@gmail.com>
2021-10-06 21:45:58 +0700

    .,

--- ExamPaper/MainPage.xaml.cs
+++ ExamPaper/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Pratice.Models;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +25,36 @@ namespace ExamPaper
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        ObservableCollection<String> imagePaths = new ObservableCollection<String>();
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void ImagesCombo_Loaded(object sender, RoutedEventArgs e)
+        {
+            string folderPath = "Assets";
+            IEnumerable<string> filePaths = await ImageService.GetAssetFiles(folderPath);
+            foreach (var filePath in filePaths)
+            {
+                imagePaths.Add(filePath);
+            }
+        }
+        private void ProductList_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProductList.Items.Add(new Product { Name = "Product", Description = "1 ", ImagePath = "Assets/1.jfif" });
+            ProductList.Items.Add(new Product { Name = "Product", Description = "2", ImagePath = "Assets/3.3.jfif" });
+            ProductList.Items.Add(new Product { Name = "Product", Description = "3", ImagePath = "Assets/33.jfif" });
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(ImagesCombo.SelectedValue);
+            ProductList.Items.Add(new Product
+            {
+                Name = NameText.Text,
+                Description = DescriptionText.Text,
+                ImagePath = (string)ImagesCombo.SelectedValue
+            });
         }
     }
 }
---
+++ ExamPaper/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+namespace Pratice.Models
+{ 
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public BitmapImage Image
+        {
+            get => new BitmapImage(new Uri("ms-appx:///" + ImagePath));
+        }
+    }
+}
